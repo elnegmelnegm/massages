f9101e6a7ad3f4f2091aa6434c41043297dd3be1
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-11 16:18:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ st.set_page_config(
     layout="wide",
 )
 
+st.markdown('''
+<img src="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png" width="250" height="100">''', unsafe_allow_html=True)
+
+st.markdown('''
+Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
+
 # Configure the generative AI API key
-genai.configure(api_key="YOUR_API_KEY")  # Replace with your actual API key
+genai.configure(api_key="AIzaSyCFPALEVIiwvWSREvVdBOzNd1VeyqQWt9o")  # Replace with your actual API key
 
 # Load the generative model
 @st.cache_resource
